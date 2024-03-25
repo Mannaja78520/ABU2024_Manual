f721be8bde37ff89c1595688844100ba0143371e
mannaja <mannaja>
2024-03-25 10:32:49 +0700

    YAY

--- lib/Robot/Robot.h
+++ lib/Robot/Robot.h
@@ -8,17 +8,17 @@
 class Robot{
   public:
     void init(){
-      Grip0.attach(Servo0);
+      // Grip0.attach(Servo0);
       // Grip1.attach(Servo1);
       // Grip2.attach(Servo2);
       // Grip3.attach(Servo3);
 
-      Grip0.write(0);
+      // Grip0.write(0);
       // Grip1.write(0);
       // Grip2.write(0);
       // Grip3.write(0);
 
-      pinMode(GripUp, OUTPUT);
+      // pinMode(GripUp, OUTPUT);
       // pinMode(Solenoid1, OUTPUT);
       // pinMode(Solenoid2, OUTPUT);
       // pinMode(Solenoid3, OUTPUT);
