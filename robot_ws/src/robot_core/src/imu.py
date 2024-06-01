ce174b770746a04425029f8fbd4dfd1ab0964ec9
mannaja <manman7852078520@gmail.com>
2024-06-01 16:36:53 +0700

    update imu

--- robot_ws/src/robot_core/src/imu.py
+++ robot_ws/src/robot_core/src/imu.py
@@ -1,16 +1,24 @@
 from mpu9250_jmdev.registers import *
 from mpu9250_jmdev.mpu_9250 import MPU9250
+import time
+
 from src.utilize import To_Radians
 
 class IMU:
     accel_data = [0.0, 0.0, 0.0]
+    velocity_data = [0.0, 0.0, 0.0]
     gyro_data = [0.0, 0.0, 0.0]
     ax = 0.0
     ay = 0.0
     az = 0.0
+    vx = 0.0
+    vy = 0.0
+    vz = 0.0
     gx = 0.0
     gy = 0.0
     gz = 0.0
+    CurrentTime = time.time()
+    LastTime = CurrentTime
     def __init__(self):
         self.mpu = MPU9250( 
             address_mpu_master=MPU9050_ADDRESS_68, # In 0x68 Address
@@ -28,16 +36,29 @@ class IMU:
         self.mpu.abias
         self.mpu.gbias
     def read(self):        
-        # This library send data as a Deg/sec
+        self.CurrentTime = time.time()
+        Dt = self.CurrentTime - self.LastTime
+        self.LastTime = self.CurrentTime
         # Read sensor data
+        # This library send Gyro_data as a Deg/sec
         self.accel_data = self.mpu.readAccelerometerMaster()
         self.gyro_data = self.mpu.readGyroscopeMaster()
         
         # filter
-        self.gyro_data[2] = 0.0 if abs(self.gyro_data[2]) < 0.5 else To_Radians(self.gyro_data[2])
         self.accel_data[0] = 0.0 if abs(self.accel_data[0]) < 0.05 else self.accel_data[0]
         self.accel_data[1] = 0.0 if abs(self.accel_data[1]) < 0.05 else self.accel_data[1]
         
+        self.gyro_data[2] = 0.0 if abs(self.gyro_data[2]) < 0.45 else To_Radians(self.gyro_data[2])
+        
+        self.velocity_data[0] += self.accel_data[0] * Dt
+        self.velocity_data[1] += self.accel_data[1] * Dt
+        self.velocity_data[2] += self.accel_data[2] * Dt
+        
+        self.velocity_data[0] = 0.0 if self.accel_data[0] == 0.0 else self.velocity_data[0]
+        self.velocity_data[1] = 0.0 if self.accel_data[1] == 0.0 else self.velocity_data[1]
+        self.velocity_data[2] = 0.0 if self.accel_data[2] == 0.0 else self.velocity_data[2]
+        
         # Store the data in class attributes
         self.ax, self.ay, self.az = self.accel_data
+        self.vx, self.vy, self.vz = self.velocity_data
         self.gx, self.gy, self.gz = self.gyro_data
