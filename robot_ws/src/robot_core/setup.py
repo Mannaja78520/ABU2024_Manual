a92adefd79d8e133ff4dafa3ae35934029ed1bc7
mannaja <manman7852078520@gmail.com>
2024-06-01 12:02:31 +0700

    fix can't colcon

--- robot_ws/src/robot_core/setup.py
+++ robot_ws/src/robot_core/setup.py
@@ -11,8 +11,8 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('config', ['config/*.yaml']),
-        (os.path.join(package_name, "src"), glob("src/*.py")),
+        (os.path.join(package_name, "src"), glob("config/*.yaml")),
+        # (os.path.join(package_name, "src"), glob("src/*.py")),
         (os.path.join("share", package_name, "launch"), glob("launch/*launch.py")),
     ],
     install_requires=['setuptools',
